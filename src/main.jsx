0898b53d27392269fb99694c0113b79a770c9609
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2026-03-17 15:40:43 +0600

    banner_update

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// import { Provider } from "react-redux";
-// import { ToastContainer } from "react-toastify";
+import { store } from './features/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>  
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
--- src/pages/homepage/banner/Banner.jsx
+++ src/pages/homepage/banner/Banner.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { Button, Input , Card, Typography, Dialog, DialogHeader, DialogBody, DialogFooter} from '@material-tailwind/react'
-
+import { useUploadbannerMutation } from '../../../features/api/exclusiveDash';
+import { useForm } from "react-hook-form"
 
 const Banner = () => {
+
+   const [uploadbanner , {isLoading , isError}] = useUploadbannerMutation();
+   const { register, handleSubmit, formState: { errors },} = useForm();
+
    const [open, setOpen] = useState(false);
    const handleOpen = () => setOpen(!open);
 
@@ -62,101 +67,108 @@ const Banner = () => {
     
   ];
 
+   const handlebanner = (data) => {
+    console.log(data.image[0])
+   }
+
   return (
     <div>
       <h1>Banner</h1>
 
       {/* Banner title & Upload Section */}
+       <form onSubmit={handleSubmit(handlebanner)}>
+          <div className='flex flex-col gap-y-5 mt-3'>
+            <div>
+                <Input size="md" label="Banner Title"  {...register("name", { required: true })}/>
+                {errors.name && <span className='text-red-400 text-[16px]  font-normal'>This field is required</span>}
+            </div>
 
-      <div className='flex flex-col gap-y-5 mt-3'>
-        <div>
-            <Input size="md" label="Banner Title" />
-        </div>
+            <div class="flex items-center  rounded-[8px] justify-center w-full">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-50 bg-[#E5E7EB] border-2 border-dashed border-gray-400 rounded-[8px] cursor-pointer hover:bg-gray-200">
+                    <div class="flex flex-col items-center justify-center text-body pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2"/></svg>
+                        <p class="mb-2 text-sm"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    </div>
+                    <input id="dropzone-file" type="file" class="hidden" {...register("image", { required: true })} />
+                    {errors.image && <span className='text-red-400 text-[16px]  font-normal'>This field is required</span>}
+                </label>
+            </div> 
 
-        <div class="flex items-center  rounded-[8px] justify-center w-full">
-            <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-50 bg-[#E5E7EB] border-2 border-dashed border-gray-400 rounded-[8px] cursor-pointer hover:bg-gray-200">
-                <div class="flex flex-col items-center justify-center text-body pt-5 pb-6">
-                    <svg class="w-8 h-8 mb-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h3a3 3 0 0 0 0-6h-.025a5.56 5.56 0 0 0 .025-.5A5.5 5.5 0 0 0 7.207 9.021C7.137 9.017 7.071 9 7 9a4 4 0 1 0 0 8h2.167M12 19v-9m0 0-2 2m2-2 2 2"/></svg>
-                    <p class="mb-2 text-sm"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                    <p class="text-xs">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                </div>
-                <input id="dropzone-file" type="file" class="hidden" />
-            </label>
-        </div> 
+            <Button type="submit" variant="outlined" loading={false} className='w-[10%]'>
+              Upload
+            </Button>
 
-        <Button variant="outlined" loading={false} className='w-[10%]'>
-          Upload
-        </Button>
-
-       {/* banner table list section */}
-      
-        <Card className="h-[275px] w-full overflow-y-scroll">
-          <table className="w-full min-w-max table-auto text-center">
-            <thead className='sticky top-0 z-10'>
-              <tr>
-                {TABLE_HEAD.map((head) => (
-                  <th
-                    key={head}
-                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                  >
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      {head}
-                    </Typography>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {TABLE_ROWS.map(({ name, job, date }, index) => {
-                const isLast = index === TABLE_ROWS.length - 1;
-                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50 text-center";
-    
-                return (
-                  <tr key={name}>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
+          {/* banner table list section */}
+          
+            <Card className="h-[275px] w-full overflow-y-scroll">
+              <table className="w-full min-w-max table-auto text-center">
+                <thead className='sticky top-0 z-10'>
+                  <tr>
+                    {TABLE_HEAD.map((head) => (
+                      <th
+                        key={head}
+                        className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                       >
-                        {name}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {job}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {date}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <div className='flex items-center gap-x-3 justify-center'>
-                        <Button color="red">Delete</Button>
-                        <Button onClick={handleOpen} color="green">Edit</Button>
-                      </div>
-                    </td>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal leading-none opacity-70"
+                        >
+                          {head}
+                        </Typography>
+                      </th>
+                    ))}
                   </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </Card>
-      </div>
+                </thead>
+                <tbody>
+                  {TABLE_ROWS.map(({ name, job, date }, index) => {
+                    const isLast = index === TABLE_ROWS.length - 1;
+                    const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50 text-center";
+        
+                    return (
+                      <tr key={name}>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {name}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {job}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {date}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <div className='flex items-center gap-x-3 justify-center'>
+                            <Button color="red">Delete</Button>
+                            <Button onClick={handleOpen} color="green">Edit</Button>
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </Card>
+          </div>
+       </form>
 
       {/* Edit modal body section  */}
        <Dialog
